7b574df2d50598cc50ae07606b1a101cc7b47297
Juan Munoz <juan.munoz@lemon42.com>
2020-06-06 16:14:06 -0500

    Configure css and image loader

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import _ from 'lodash';
+import './styles.css';
+import Icon from './myimage.png';
+
   function component() {
     const element = document.createElement('div');
   // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = './dist/' + Icon;
+    element.appendChild(myIcon);
     return element;
   }
   document.body.appendChild(component());
